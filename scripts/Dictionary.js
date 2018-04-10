30a0dc91f2e148d62658555447aa9c9488c93848
JJINGW <15501108688@163.com>
2018-04-10 21:21:55 +0800

    add Dictionary

---
+++ scripts/Dictionary.js
@@ -0,0 +1,84 @@
+/*
+* @Author: wangjing
+* @Date:   2018-04-10 18:43:43
+* @Last Modified by:   wangjing
+* @Last Modified time: 2018-04-10 19:17:28
+*/
+// 字典存储不重复的[键，值]对，键名是用来查询特定元素的，也称为映射map。
+function Dictionary() {
+	var items = {};
+
+	// 查看字典中是否有某个键
+	this.has = function(key) {
+		return key in items;
+	};
+
+	// 向字典中添加元素
+	this.set = function(key, value) {
+		items[key] = value;
+	};
+
+	// 使用键值来移除数据
+	this.delete = function(key) {
+		if (this.has(key)) {
+			delete items[key];
+			return true;
+		}
+		return false;
+	};
+
+	// 查找指定项的值
+	this.get = function(key) {
+		return this.has(key) ? items[key] : undefined;
+	};
+
+	// 返回字典中所有values实例的值
+	this.values = function() {
+		var values = [];
+		for (var k in items) {
+			if (this.has(k)) {
+				values.push(items[k]);
+			}
+ 		}
+
+ 		return values;
+	};
+
+	this.clear = function() {
+		items = {};
+	};
+
+	this.size = function() {
+		let count = 0;
+		for (var k in items) {
+			if (items.hasOwnProperty(k)) ++count;
+		}
+		return count;
+	};
+
+	// 取得字典中所有的键名
+	this.keys = function() {
+		return Object.keys(items);
+	};
+
+	this.getItems = function() {
+		return items;
+	};
+
+}
+
+var dictionary = new Dictionary();
+dictionary.set('a','a.com');
+dictionary.set('b','b.com');
+dictionary.set('c','c.com');
+
+console.log(dictionary.has('a'));
+console.log(dictionary.size());
+console.log(dictionary.keys());
+console.log(dictionary.values());
+console.log(dictionary.get('a'));
+dictionary.delete('b');
+console.log(dictionary.keys());
+console.log(dictionary.values());
+console.log(dictionary.getItems());
+
